a23ea5af9cf245c870291c3bf897c464cc376542
Rimki <yhs693@naver.com>
2018-06-14 06:15:01 +0900

    .

--- NoticeList.php
+++ NoticeList.php
@@ -3,8 +3,8 @@
   $con  = mysqli_connect("localhost","eun","Abcd23@#","multiple");
   mysqli_set_charset($con,"utf8");
   //$id=$_GET["id"];
-  $id="test";
-  $result = mysqli_query($con,"SELECT address,item FROM soongil_".$id);
+
+  $result = mysqli_query($con,"SELECT address,item FROM soongil_test);
   $response = array();
 
   while($row = mysqli_fetch_array($result)){
