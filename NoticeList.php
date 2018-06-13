e08aa7afea5ba9e15282f42dc726c6657b3bb2a1
Rimki <yhs693@naver.com>
2018-06-14 05:50:12 +0900

    .

--- NoticeList.php
+++ NoticeList.php
@@ -1,9 +1,9 @@
 <?php
   header("Content-Type: text/html;charset=UTF-8");
   $con  = mysqli_connect("localhost","eun","Abcd23@#","multiple");
-  mysqli_set_charset($con,"utf8"); 
+  mysqli_set_charset($con,"utf8");
   $id=$_GET["id"];
-  $result = mysqli_query($con,"SELECT address,item FROM soongil_".$id."WHERE address=? AND item=?");
+  $result = mysqli_query($con,"SELECT address,item FROM soongil_".$id);
   $response = array();
 
   while($row = mysqli_fetch_array($result)){
