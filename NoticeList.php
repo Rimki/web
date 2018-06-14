9ca3cf3b22add297b4a05cef999539404610c3b0
Rimki <yhs693@naver.com>
2018-06-14 20:31:18 +0900

    .

--- NoticeList.php
+++ NoticeList.php
@@ -1,7 +1,7 @@
 <?php
 //  header("Content-Type: text/html;charset=UTF-8");
   $con  = mysqli_connect("localhost","eun","Abcd23@#","multiple");
-//  mysqli_set_charset($con,"ANCI");
+  mysqli_set_charset($con,"utf-8");
   //$id=$_GET["id"];
 
   $result = mysqli_query($con,"SELECT address,item FROM soongsil_test;");
