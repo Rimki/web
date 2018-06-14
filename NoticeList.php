df5dbd56af04d4d16e3d25cd140b7c24a032d4e4
Rimki <yhs693@naver.com>
2018-06-14 16:54:01 +0900

    .

--- NoticeList.php
+++ NoticeList.php
@@ -10,7 +10,7 @@
   while($row = mysqli_fetch_array($result)){
       array_push($response,array("address"->$row[0],"item"->$row[1]));
   }
-  echo json_encode(array("response"->$response);
+  echo json_encode(array("response"->$response),JSON_PRETTY_PRINT+JSON_UNESCAPED_UNICODE);
   mysqli_close($con);
 
 ?>
