3e38fcf4fac4f4fae6a6f626d819d5c22da9b007
eunwonki <yony1995@naver.com>
2018-06-21 23:06:33 +0900

    delete.php 추가

---
+++ delete.php
@@ -0,0 +1,15 @@
+<?php
+  $con  = mysqli_connect("localhost","eun","Abcd23@#","multiple");
+  mysqli_set_charset($con,"utf-8");
+  $aid=$_POST["aid"];
+
+  for($i = 0; $i < count(aid); $i++)
+  {
+	  mysqli_prepare($con,"DELETE FROM soongsil_delivery WHERE aid=?");
+	  mysqli_stmt_bind_param($statement,"s",$aid[i]);
+	  mysqli_stmt_execute($statement);
+  }
+
+  mysqli_close($con);
+  echo "<script>location.replace('main.html');</script>";
+?>
