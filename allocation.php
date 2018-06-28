f56297d384572b50f52c7f7f1d6a367dc2eb4b76
eunwonki <yony1995@naver.com>
2018-06-28 23:22:23 +0900

    완성

--- allocation.php
+++ allocation.php
@@ -13,11 +13,34 @@
 	
 	$len = count($id_arr);
 
+
 	for ( $i =0 ; $i < $len; $i++){
-		//$old_query = "select ";
-		echo "$id_arr[$i] $val_arr[$i]<br>";
+		$id = $id_arr[$i];
+		$val = $val_arr[$i];
+		$is_alloc = "select allocate, aid, address, item from soongsil_delivery where aid=$id";
+		$result_set = mysqli_query($conn, $is_alloc);
+
+		while($row = mysqli_fetch_array($result_set)){
+			if ($row[0] == ''){
+				$alloc_update = "update soongsil_delivery set allocate='$val' where aid=$id";
+				mysqli_query($conn, $alloc_update);
+				
+				$alloc_insert = "insert into soongsil_$val(aid, address, item) values('$row[1]','$row[2]','$row[3]')";
+				mysqli_query($conn, $alloc_insert);
+			}
+			else {
+				$alloc_delete = "delete from soongsil_$row[0] where aid = $id";
+				mysqli_query($conn, $alloc_delete);
+
+				$alloc_update = "update soongsil_delivery set allocate='$val' where aid=$id";
+				mysqli_query($conn, $alloc_update);
+				
+				$alloc_insert = "insert into soongsil_$val(aid, address, item) values('$row[1]','$row[2]','$row[3]')";
+				mysqli_query($conn, $alloc_insert);
+			}
+		}
 		
 	}
 
-	//echo "<script>location.replace('main.html');</script>";	
+	echo "<script>location.replace('main.html');</script>";	
 ?>
